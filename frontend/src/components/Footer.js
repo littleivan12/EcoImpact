dab3301f89f1b3974c4577102b5e194292c44195
quintusManus <107276939+quintusManus@users.noreply.github.com>
2025-04-03 10:57:14 -0400

    took away the footer content string on every footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ function FooterBar(){
     return(
         <footer class="footer-bar">
             <div class="footer-content">
-                <h1>[Footer Content]</h1>
+                <h1>[]</h1>
             </div>
         </footer>
     );
