6a91d0b1be53330b28b89b2c54e49d1fa05ee44a
Ivan Candelero <i_ramoscand@uncg.edu>
2025-07-09 19:59:56 -0400

    cleaning up db stuff

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-#DATABASE_URL = "postgresql://ivancandelero:@localhost:5432/ecoimpact"  # Corrected PostgreSQL URL
-DATABASE_URL = "postgresql://postgres:hivemind@localhost:5432/ecoimpact"  
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)  # Removed `connect_args`
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
